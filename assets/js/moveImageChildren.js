7ae3aa48fed10e36ce52e7fe3b14e10fcc9a097a
Nguyễn Xuân Đoàn <88759481+doandc701@users.noreply.github.com>
2022-06-04 22:12:50 +0700

    imgChildren
    chuyển ảnh con lên ảnh cha khi click

---
+++ assets/js/moveImageChildren.js
@@ -0,0 +1,8 @@
+$(()=>{
+    $('.imgchild img').click(function(){
+        // lấy đường dẫn của ảnh click
+        let imgPath= $(this).attr('src')
+        // gọi đến ảnh chính và thay đổi ảnh khi click
+        $('#imgproduceParent').attr('src', imgPath)
+    })
+})
