5f98fbb586dee7b801702f5571e228248bd9dde7
Nguyễn Xuân Đoàn <88759481+doandc701@users.noreply.github.com>
2022-06-05 09:09:50 +0700

    Update popUp.js

--- assets/js/popUp.js
+++ assets/js/popUp.js
@@ -3,6 +3,6 @@ const btn = document.querySelector(".btn-add");
 btn.onclick = function (e) {
   modals.classList.add("active");
   setTimeout(function () {
-    location.href = "./detail.html";
+    location.href = "./index.html";
   }, 1500);
 };
