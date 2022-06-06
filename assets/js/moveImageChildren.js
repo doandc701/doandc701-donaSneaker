3c3633a3c4fd6eb5f1831c7ad24fb897201ae3d5
Nguyễn Xuân Đoàn <88759481+doandc701@users.noreply.github.com>
2022-06-06 21:30:21 +0700

    Update moveImageChildren.js

--- assets/js/moveImageChildren.js
+++ assets/js/moveImageChildren.js
@@ -1,5 +1,5 @@
 $(()=>{
-    $('.imgchild img').click(function(){
+    $('.thumbnail').click(function(){
         // lấy đường dẫn của ảnh click
         let imgPath= $(this).attr('src')
         // gọi đến ảnh chính và thay đổi ảnh khi click
